6496743e7495e25896806e8ac59f61ccab9b5739
Zama Braga <zamabandeirabr@gmail.com>
2023-05-01 14:44:05 -0300

    feat: add log de remoção dos arquivos

--- pragprog.com/rggo/workingFiles/walk/main.go
+++ pragprog.com/rggo/workingFiles/walk/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -13,23 +14,39 @@ type config struct {
 	size int64
 	list bool
 	del  bool
+	wlog io.Writer
 }
 
 func main() {
-
 	root := flag.String("root", ".", "Root directory to start")
 	list := flag.Bool("list", false, "List file only")
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
 	del := flag.Bool("dell", false, "Delete files")
+	logFile := flag.String("log", "value string", "Log deletes to this file")
 
 	flag.Parse()
+
+	var (
+		f   = os.Stdout
+		err error
+	)
+
+	if *logFile != "" {
+		f, err = os.OpenFile(*logFile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
 
 	c := config{
 		ext:  *ext,
 		size: *size,
 		list: *list,
 		del:  *del,
+		wlog: f,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
@@ -39,6 +56,9 @@ func main() {
 }
 
 func run(root string, out io.Writer, cfg config) error {
+
+	delLogger := log.New(cfg.wlog, "DELETED FILE: ", log.LstdFlags)
+
 	return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return nil
@@ -53,7 +73,7 @@ func run(root string, out io.Writer, cfg config) error {
 		}
 
 		if cfg.del {
-			return delFile(path)
+			return delFile(path, delLogger)
 		}
 
 		return listFile(path, out)
